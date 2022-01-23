0e49a26a200cb27b0e8fc6b2a47f441767c45249
CmAlam1 <yourd725@gmail.com>
2022-01-23 06:38:13 -0800

    completing apply and call

--- callApply.js
+++ callApply.js
@@ -1,3 +1,7 @@
+// jokhon kono akta object er moddhe kono akta method asea sheata oi object ea kora bade 
+// exactly shei method ta jodi onno akta object ea apply korte chai shea khetre amra bind kore felbo
+
+
 const normalPerson = {
     firstName : 'Rahim',
     lastName : 'Uddin',
@@ -5,8 +9,8 @@ const normalPerson = {
     getFullName : function (amount){
         console.log(this.firstName, this.lastName);
     },
-    chargeBill : function (amount){
-        this.salary = this.salary - amount;
+    chargeBill : function (amount, tip, tax){
+        this.salary = this.salary - amount - tip - tax;
         return this.salary;
     }
 }
@@ -23,15 +27,34 @@ const friendlyPerson = {
     salary : 25000,
 }
 
-const heroBillCharge = normalPerson.chargeBill.bind(heroPerson);
-heroBillCharge(2000);
+// const heroBillCharge = normalPerson.chargeBill.bind(heroPerson);
+// heroBillCharge(2000);
 
-const friendlyBillCharge = normalPerson.chargeBill.bind(friendlyPerson);
-friendlyBillCharge(5000);
+// const friendlyBillCharge = normalPerson.chargeBill.bind(friendlyPerson);
+// friendlyBillCharge(5000);
 
+// console.log(heroPerson.salary);
+
+// console.log(friendlyPerson.salary);
+
+
+
+// call (shortcut) = direct call kore onno kono object er method re. call er por , dite hobe
+
+// normalPerson.chargeBill.call(heroPerson, 1000, 100, 50);
+// normalPerson.chargeBill.call(heroPerson, 3000, 200, 30);
+// console.log(heroPerson.salary);
+
+
+// normalPerson.chargeBill.call(friendlyPerson, 500, 30, 10);
+// console.log(friendlyPerson.salary);
+
+// console.log(normalPerson.salary);
+
+
+// apply (call er khalato vai) = same as call er moto use kore jst perameter er por array te value gula rakhte hobe thats it
+
+normalPerson.chargeBill.apply(heroPerson, [3000, 50, 10]);
 console.log(heroPerson.salary);
 
-console.log(friendlyPerson.salary);
 
-// jokhon kono akta object er moddhe kono akta method asea sheata oi object ea kora bade 
-// exactly shei method ta jodi onno akta object ea apply korte chai shea khetre amra bind kore felbo
